561b08c80b64f773870fa693b04e756d6e5dfc18
ksj0818280 <ksj0818280@naver.com>
2021-07-19 09:49:22 +0900

    7/19 5ban class commit

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
 
 app_name = 'accountapp'
 
@@ -13,7 +13,9 @@ urlpatterns = [
          name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 
-    path('create/', AccountCreateView.as_view(), name='create'),
+    path('create/', AccountCreateView   .as_view(), name='create'),
 
-    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail')
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
+
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update')
 ]
