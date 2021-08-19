c237e9ee01573ed3674c1606eff09eadb62c3da9
ksj0818280 <ksj0818280@naver.com>
2021-08-19 10:09:06 +0900

    8/19 5ban class commit

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy, reverse
@@ -6,9 +7,9 @@ from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 from django.views.generic.edit import FormMixin
 
+from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
-from articleapp.decorators import article_ownership_required
 from commentapp.forms import CommentCreationForm
 
 
@@ -22,6 +23,10 @@ class ArticleCreateView(CreateView):
     def form_valid(self, form):
         form.instance.writer = self.request.user
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
+
 
 class ArticleDetailView(DetailView, FormMixin):
     model = Article
@@ -44,7 +49,7 @@ class ArticleUpdateView(UpdateView):
 
 @method_decorator(article_ownership_required, 'get')
 @method_decorator(article_ownership_required, 'post')
-class  ArticleDeleteView(DeleteView):
+class ArticleDeleteView(DeleteView):
     model = Article
     context_object_name = 'target_article'
     success_url = reverse_lazy('articleapp:list')
@@ -56,5 +61,3 @@ class ArticleListView(ListView):
     context_object_name = 'article_list'
     template_name = 'articleapp/list.html'
     paginate_by = 20
-
-
