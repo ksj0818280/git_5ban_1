4b90b3bdb70b8e1b3f225d26d987406c5d9a3084
ksj0818280 <ksj0818280@naver.com>
2021-08-24 16:42:05 +0900

    8/24 5ban class commit

--- likeapp/urls.py
+++ likeapp/urls.py
@@ -5,5 +5,5 @@ from likeapp.views import LikeArticleView
 app_name = 'likeapp'
 
 urlpatterns = [
-    path('article/<int:article_pk>', LikeArticleView.as_view(), name='article_list'),
+    path('article/<int:article_pk>', LikeArticleView.as_view(), name='article_like'),
 ]
